adf14bc0f554df8651f24f004e306ed151d77e31
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-02-26 22:34:51 -0500

    keywords_tkinter

--- textos.py
+++ textos.py
@@ -3,7 +3,10 @@ from tkinter import *
 ventana = Tk()
 ventana.geometry("500x500")
 
-texto = Label(ventana,text="Bienvenido a mi programa")
+def pruebas(nombre,apellidos, pais):
+    return f"Hola {nombre} {apellidos} veo que eres de {pais}"
+
+texto = Label(ventana,text=pruebas(nombre="Daniel",apellidos="OSorio",pais="Colombia"))
 texto.config(fg="white", bg="black", padx=20, pady=20, font=("Arial", 15), cursor="spider")
 texto.pack()
 
