876afe4e51d5a15b71657b1a8253a310697fb61e
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-03-11 21:49:25 -0500

    ejercicio_tkinter

--- ejercicio.py
+++ ejercicio.py
@@ -9,6 +9,27 @@ numero1 = StringVar()
 numero2 = StringVar()
 resultado = StringVar()
 
+def sumar():
+    resultado.set(float(numero1.get()) + float(numero2.get()))
+    mostrarResultado()
+
+def restar():
+    resultado.set(float(numero1.get()) - float(numero2.get()))
+    mostrarResultado()
+
+def multiplicar():
+    resultado.set(float(numero1.get()) * float(numero2.get()))
+    mostrarResultado()
+
+def dividir():
+    resultado.set(float(numero1.get()) / float(numero2.get()))
+    mostrarResultado()
+
+def mostrarResultado():
+    messagebox.showinfo("Resultado: ", f"El resultado de la operacion es: {resultado.get()}")
+    numero1.set("")
+    numero2.set("")
+
 marco = Frame(ventana, width=250, height=250)
 marco.config(
     bd=5,
@@ -23,10 +44,10 @@ Entry(marco, textvariable=numero1, justify="center").pack()
 Label(marco, text="Segundo numero").pack()
 Entry(marco, textvariable=numero2, justify="center").pack()
 
-Button(marco, text="Sumar").pack()
-Button(marco, text="Restar").pack()
-Button(marco, text="multiplicar").pack()
-Button(marco, text="Dividir").pack()
+Button(marco, text="Sumar", command=sumar).pack()
+Button(marco, text="Restar",command=restar).pack()
+Button(marco, text="multiplicar",command=multiplicar).pack()
+Button(marco, text="Dividir",command=dividir).pack()
 
 
 ventana.mainloop()
