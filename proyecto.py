b7d2c2bb8d9142102e2fa45fde2c5d5038dc1973
danielosorio0224 <dosorioquimbayo@gmail.com>
2024-03-19 10:46:26 -0500

    proyecto_tkinter

--- proyecto.py
+++ proyecto.py
@@ -23,6 +23,7 @@ def home():
     return True
 
 def add():
+    #encabezado
     addLabel.config(
         fg="white",
         bg="black",
@@ -30,7 +31,17 @@ def add():
         padx=20,
         pady=20
     )
-    addLabel.grid(row=0, column=0)
+    addLabel.grid(row=0, column=0, columnspan=4)
+
+    #campos formulario
+    add_name_label.grid(row=1,column=0)
+    add_name_entry.grid(row=1, column=1)
+    add_price_label.grid(row=2,column=0)
+    add_price_entry.grid(row=2,column=1)
+    add_descripcion_label.grid(row=3,column=0, sticky=NW)
+    add_descripcion_entry.grid(row=3,column=1)
+    add_descripcion_entry.config(width=30, height=5, font=("Consolas",12),padx=15,pady=15)
+    boton.grid(row=4,column=1)
 
     homeLabel.grid_remove()
     infoLabel.grid_remove()
@@ -52,8 +63,25 @@ def info():
     homeLabel.grid_remove()
     return True
 
+#variables importante
+nameData = StringVar()
+priceData = StringVar()
+
 homeLabel = Label(ventana, text="Inicio")
 addLabel = Label(ventana, text="Añadir producto")
+
+#Campos formulario
+add_name_label = Label(ventana, text="Nombre: ")
+add_name_entry = Entry(ventana, textvariable=nameData)
+
+add_price_label = Label(ventana, text="Precio: ")
+add_price_entry = Entry(ventana, textvariable=priceData)
+
+add_descripcion_label = Label(ventana, text="Descripcion: ")
+add_descripcion_entry = Text(ventana)
+
+boton = Button(ventana, text="Guardar")
+
 infoLabel = Label(ventana, text="Informacion producto")
 dataLabel = Label(ventana,text="Creado por daniel")
 
